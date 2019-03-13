b34f1c1dd97512e83d0c436b459daa1c8220978a
Setya Wibawa <durianpeople@gmail.com>
2019-03-13 21:26:00 +0700

    update

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -7,36 +7,85 @@
 #include <string.h>
 #include <sys/stat.h>
 
-int main() {
+int main()
+{
+    pid_t pid, sid;
+
+    pid = fork();
+
+    if (pid < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid > 0)
+    {
+        exit(EXIT_SUCCESS);
+    }
+
+    umask(0);
+
+    sid = setsid();
+
+    if (sid < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    if ((chdir("/")) < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    while (1)
+    {
+        // main program here
+        sleep(30);
+    }
+
+    exit(EXIT_SUCCESS);
+}
+
+void main2()
+{
     pid_t child_id;
     struct dirent *de;
     int status;
     char *ptr, *before;
-    DIR *dr=opendir(".");
-    if (dr==NULL) {
+    DIR *dr = opendir(".");
+    if (dr == NULL)
+    {
         printf("Gagal buka direktori\n");
         return 0;
     }
-    while ((de=readdir(dr))!=NULL) {
-        
-        child_id=fork();
-        if (child_id==0) {
+    while ((de = readdir(dr)) != NULL)
+    {
+
+        child_id = fork();
+        if (child_id == 0)
+        {
             //child
-            char* ext=strstr(de->d_name, ".png");
-            int stringlength=strlen(de->d_name);
-            int pointer=(ext-de->d_name);
-            int trueext=(stringlength-pointer==4);
-            if (trueext) {
+            char *ext = strstr(de->d_name, ".png");
+            int stringlength = strlen(de->d_name);
+            int pointer = (ext - de->d_name);
+            int trueext = (stringlength - pointer == 4);
+            if (trueext)
+            {
                 printf("True Ext\n");
-                char basename[pointer+1];
-                memcpy(basename, &de->d_name[0], pointer );
+                char basename[pointer + 1];
+                memcpy(basename, &de->d_name[0], pointer);
                 basename[pointer] = '\0';
                 //https://stackoverflow.com/questions/7430248/creating-a-new-directory-in-c
                 struct stat st = {0};
-                if (stat("gambar", &st) == -1) {
+                if (stat("gambar", &st) == -1)
+                {
                     mkdir("gambar", 0777);
                 }
-                char target[1000]="./gambar/";
+                char target[1000] = "./gambar/";
                 strcat(target, basename);
                 strcat(target, "_grey.png");
                 // char* target=strcat(basename, "_grey.png");
@@ -45,12 +94,13 @@ int main() {
             }
             exit(0);
         }
-        else {
+        else
+        {
             //parent
-            while(wait(&status)>0);
+            while (wait(&status) > 0)
+                ;
             continue;
         }
     }
     closedir(dr);
-    return 0;
 }
