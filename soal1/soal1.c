0970dab26ad2233aa4eaefb927e9a03866c16a51
Setya Wibawa <durianpeople@gmail.com>
2019-03-13 18:31:07 +0700

    update

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -1,24 +1,49 @@
 #include <dirent.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
 
 int main() {
     pid_t child_id;
     struct dirent *de;
     int status;
+    char *ptr, *before;
     DIR *dr=opendir(".");
     if (dr==NULL) {
         printf("Gagal buka direktori\n");
         return 0;
     }
     while ((de=readdir(dr))!=NULL) {
-        // printf("%s\n", de->d_name);
+        
         child_id=fork();
         if (child_id==0) {
             //child
-            char *argv[4] = {"mv", de->d_name, "sample-dir", NULL};
-            execv("/bin/mkdir", argv);
+            char* ext=strstr(de->d_name, ".png");
+            int stringlength=strlen(de->d_name);
+            int pointer=(ext-de->d_name);
+            int trueext=(stringlength-pointer==4);
+            if (trueext) {
+                printf("True Ext\n");
+                char basename[pointer+1];
+                memcpy(basename, &de->d_name[0], pointer );
+                basename[pointer] = '\0';
+                //https://stackoverflow.com/questions/7430248/creating-a-new-directory-in-c
+                struct stat st = {0};
+                if (stat("gambar", &st) == -1) {
+                    mkdir("gambar", 0777);
+                }
+                char target[1000]="./gambar/";
+                strcat(target, basename);
+                strcat(target, "_grey.png");
+                // char* target=strcat(basename, "_grey.png");
+                printf("Execv %s to %s\n", de->d_name, target);
+                rename(de->d_name, target);
+            }
+            exit(0);
         }
         else {
             //parent
@@ -26,7 +51,6 @@ int main() {
             continue;
         }
     }
-
     closedir(dr);
     return 0;
 }
